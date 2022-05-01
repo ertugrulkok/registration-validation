99b8b10951a138ef6043537a58134dc665f2937d
ertugrulkok <101091726+ertugrulkok@users.noreply.github.com>
2022-05-01 19:20:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
 
   let container = document.getElementById("extra_container");
   let mainContainer = document.getElementById("mainContainer");
-
-function newUser() {
-  let name = document.getElementById("name").value;
-  let sirName = document.getElementById("sirname").value;
-  let addres = document.getElementById("addres").value;
-  let stateStand = document.getAnimations("state");
-  let cityStand = document.getAnimations("city");
-  let dob = document.getElementById("dob").value;
-  let pinCode = document.getElementById("pincode").value;
-  let course = document.getElementById("course");
-  let email = document.getElementById("e_mail").value;
-  let info = document.getElementsByClassName("input")
+  const stForm= document.getElementById("StudentRegistrationForm");
 
 
-  text.innerHTML = "CONGRATULATIONS!" + " " + name;
-  nameid.innerHTML = name;
-  sirnameid.innerHTML = sirName;
-  stateid.innerHTML = state.options[state.selectedIndex].textContent;
-  addresid.innerHTML = addres;
-  cityid.innerHTML = city.options[city.selectedIndex].textContent;
-  courseid.innerHTML = course.options[course.selectedIndex].textContent;
-  dobid.innerHTML = dob;
-  pinid.innerHTML = pinCode;
-  emailid.innerHTML = email;
-
-  container.style.display = "flex";
-
-  mainContainer.style.opacity = "20%";
-
-  if (male.checked) {
-    maleid.innerHTML = "male";
-  } else if (female.checked) {
-    maleid.innerHTML = "female";
-  }
-}
+  stForm.addEventListener("submit",function(event) { 
+  
+    let name = document.getElementById("name").value;
+    let sirName = document.getElementById("sirname").value;
+    let addres = document.getElementById("addres").value;
+    let stateStand = document.getAnimations("state");
+    let cityStand = document.getAnimations("city");
+    let dob = document.getElementById("dob").value;
+    let pinCode = document.getElementById("pincode").value;
+    let course = document.getElementById("course");
+    let email = document.getElementById("e_mail").value;
+  
+    
+    
+    text.innerHTML = "CONGRATULATIONS!" + " " + name;
+    nameid.innerHTML = name;
+    sirnameid.innerHTML = sirName;
+    stateid.innerHTML = state.options[state.selectedIndex].textContent;
+    addresid.innerHTML = addres;
+    cityid.innerHTML = city.options[city.selectedIndex].textContent;
+    courseid.innerHTML = course.options[course.selectedIndex].textContent;
+    dobid.innerHTML = dob;
+    pinid.innerHTML = pinCode;
+    emailid.innerHTML = email;
+ 
+    
+    container.style.display = "flex";
+    mainContainer.style.opacity = "20%";
+    
+    if (male.checked) {
+      maleid.innerHTML = "male";
+    } else if (female.checked) {
+      maleid.innerHTML = "female";
+    }
+      event.preventDefault();
+      
+    
+      
+      });
 
 function resetAll() {
     container.style.display ="none";
     mainContainer.style.opacity = "100";
-    value.info(remove)
+    document.getElementById("StudentRegistrationForm").reset();
     
 }
